7add33b4703990984b8026176b0d114ae3ee274c
Winexwolf <anukriti.jaiswal01@gmail.com>
2023-11-28 01:57:32 -0500

    added browser router, updated router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BottomNavbar from "./js/navbar";
 import Mood0 from "./js/mood0";
 import Mood1 from "./js/mood1";
-import Mood2 from "./js/mood2"; 
+import Mood2 from "./js/mood2";
 import Mood3 from "./js/mood3";
 
 function App() {
@@ -12,36 +12,22 @@ function App() {
     console.log("Cancel button clicked!");
     // Add your cancel logic here
   };
+
   return (
     <Router>
       <>
-        <div className="App"></div>
-        {/*         <TitleBar showBackButton />
-         */}{" "}
-        <Switch>
-          <Route path="/mood" component={Mood0} />
-          <Route path="/mood1" component={Mood1} />
-          <Route path="/mood2" component={Mood2} />
-          <Route path="/mood3" component={Mood3} />
-        </Switch>
-        <BottomNavbar />
+        <div className="App">
+          {/* Add your other components or content here */}
+          <Routes>
+            <Route path="/mood" element={<Mood0 />} />
+            <Route path="/mood1" element={<Mood1 />} />
+            <Route path="/mood2" element={<Mood2 />} />
+            <Route path="/mood3" element={<Mood3 />} />
+          </Routes>
+          <BottomNavbar />
+        </div>
       </>
     </Router>
-    // <Router>
-
-    // {/*
-    //         {/*             <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    //          */}
-    //         {/*  <Modal
-    //           modalText="Your Mood Data is Successfully Shared on WhatsApp"
-    //           showCancelButton
-    //           onCancel={handleCancel}
-    //         /> */}
-    //         <TitleBar showBackButton />
-
-    //         <BottomNavbar />
-    //       </div>
-    //           </Router> */}
   );
 }
 
