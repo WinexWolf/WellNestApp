37f366e4a3066d64549afa5c4c15f1a9b9fbde9f
YIYANG HUO <2392314079@qq.com>
2023-12-03 23:09:00 -0800

    use mui-styles instead of @mui/styles to fix dependency issue

--- src/components/therapymatching/Loading.jsx
+++ src/components/therapymatching/Loading.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@mui/styles';
-
+//import { makeStyles } from '@mui/styles';
+import { makeStyles } from "mui-styles";
 
 // Define custom styles
 const useStyles = makeStyles({
