1876282379d58e146689855e3d71334fdb2d7404
Winexwolf <anukriti.jaiswal01@gmail.com>
2023-12-04 12:17:49 -0800

    added some landing pg anaimation and changed ui

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,21 +1,38 @@
+import React from "react";
+import { useSpring, animated, config } from "react-spring";
 import Button from "@mui/material/Button";
 
 const Root = () => {
+  const imageAnimation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    delay: 1200,
+    config: config.gentle, // Use a gentle easing function
+  });
+
+  const textAnimation = useSpring({
+    opacity: 1,
+    marginTop: 0,
+    from: { opacity: 0, marginTop: -50 },
+    delay: 2000,
+    config: config.gentle, // Use a gentle easing function
+  });
+
   return (
     <div className="h-screen">
       {/* IMAGE */}
-      <div className="flex justify-center mb-[58px]">
+      <animated.div className="flex justify-center w-30 h-30 mt-20 mb-[20px]" style={imageAnimation}>
         <img
           src="images/WellNestIcon_transparent.png"
           alt="WellNest icon"
-          className="max-w-full mb-8 "
+          className="max-w-full mb-8"
         ></img>
-      </div>
+      </animated.div>
       {/* Text Section */}
-      <div className="grid text-center lg-mx-32">
-        <div className="font-extrabold text-[60px]">WellNest</div>
-        <div className="font-semibold text-[40px]">Embrace Wellness</div>
-        <div className="font-semibold text-[40px] mb-[58px]">
+      <animated.div className="grid text-center lg-mx-32" style={textAnimation}>
+        <div className="font-extrabold text-[60px] mb-5">WellNest</div>
+        <div className="font-semibold text-[25px]">Embrace Wellness</div>
+        <div className="font-semibold text-[25px] mb-[58px]">
           Embrace WellNest
         </div>
         {/* Start button */}
@@ -23,12 +40,12 @@ const Root = () => {
           <Button
             variant="contained"
             href="/features"
-            style={{ backgroundColor: "#0087E8" }}>
-            {" "}
-            Start{" "}
+            style={{ backgroundColor: "#0087E8" }}
+          >
+            Start
           </Button>
         </div>
-      </div>
+      </animated.div>
     </div>
   );
 };
